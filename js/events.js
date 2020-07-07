98a56930aac09f43bf648cd4e8df405843cd7f37
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-07-07 19:11:24 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,6 +11,13 @@ function frameIt(){
   });
 }
 
+function pressIt(){
+  $("input").on("keydown", function(key){
+    if (key.which === 71){
+      window.alert("G was pressed.");
+    }
+  })
+}
 
 
 
